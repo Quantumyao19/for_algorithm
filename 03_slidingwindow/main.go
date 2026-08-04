844edaa1ef51d030acf15ebaac57c7f64a4e4df2
JunYao <quantumyao19@outlook.com>
2026-08-04 11:05:11 +0800

    practice algorithm

--- 03_slidingwindow/main.go
+++ 03_slidingwindow/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "slices"
+
 func main() {}
 
 // Day 1: Longest Substring Without Repeating Characters
@@ -17,4 +19,54 @@ func LengthOfLongestSubstring(s string) int {
 
 	}
 	return res
+}
+
+// Day 2: Permutation in String
+func PermutationInString(s1 string, s2 string) bool {
+	if len(s1) > len(s2) {
+		return false
+	}
+
+	left := 0
+	s1byte := []byte(s1)
+	slices.Sort(s1byte)
+	for right := len(s1) - 1; right < len(s2); right++ {
+		s2byte := []byte(s2[left : right+1])
+		slices.Sort(s2byte)
+		if string(s1byte) == string(s2byte) {
+			return true
+		}
+		left++
+	}
+	return false
+}
+
+func PermutationInString2(s1 string, s2 string) bool {
+	if len(s1) > len(s2) {
+		return false
+	}
+
+	var count1 [26]int
+	var count2 [26]int
+
+	for i := range s1 {
+		count1[s1[i]-'a']++
+		count2[s2[i]-'a']++
+	}
+
+	if count1 == count2 {
+		return true
+	}
+
+	for right := len(s1); right < len(s2); right++ {
+		count2[s2[right]-'a']++
+
+		count2[s2[right-len(s1)]-'a']--
+
+		if count1 == count2 {
+			return true
+		}
+	}
+
+	return false
 }
